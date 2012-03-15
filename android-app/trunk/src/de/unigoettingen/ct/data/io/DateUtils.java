d1189b7e25712feefe2df46428bf8af130b16b85
Fabian.Sudau@googlemail.com <Fabian.Sudau@googlemail.com@ab771393-dd29-93c4-2591-7105257eade2>
2012-03-15 14:21:34 +0000

    Improved the time-resolution to include milliseconds. Removed some dead code while doing so.

--- android-app/trunk/src/de/unigoettingen/ct/data/io/DateUtils.java
+++ android-app/trunk/src/de/unigoettingen/ct/data/io/DateUtils.java
@@ -1,10 +1,7 @@
 package de.unigoettingen.ct.data.io;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 /**
  * Provides static  utility for converting from Calendar objects to Strings and vice versa.
@@ -14,8 +11,8 @@ import java.util.GregorianCalendar;
  */
 public class DateUtils {
 	
-	// example date: 2011/09/23 13:42:01
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	// example date: 2011/09/23 13:42:01:438
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:S");
 
 	private DateUtils() {
 
@@ -23,14 +20,6 @@ public class DateUtils {
 
 	public synchronized static String calendarToString(Calendar calendar) {
 		return DATE_FORMAT.format(calendar.getTime());
-	}
-
-	
-	public synchronized static GregorianCalendar parseTimestamp(String timestamp) throws ParseException{
-		Date d = DATE_FORMAT.parse(timestamp);
-		GregorianCalendar ret = new GregorianCalendar();
-		ret.setTime(d);
-		return ret;
 	}
 
 }
--- android-app/trunk/src/de/unigoettingen/ct/upload/CalendarTransformer.java
+++ android-app/trunk/src/de/unigoettingen/ct/upload/CalendarTransformer.java
@@ -1,11 +1,7 @@
 package de.unigoettingen.ct.upload;
 
-import java.lang.reflect.Type;
-import java.text.ParseException;
 import java.util.Calendar;
 
-import flexjson.ObjectBinder;
-import flexjson.ObjectFactory;
 import flexjson.transformer.AbstractTransformer;
 
 /**
@@ -14,21 +10,10 @@ import flexjson.transformer.AbstractTransformer;
  * @author Fabian Sudau
  *
  */
-public class CalendarTransformer extends AbstractTransformer implements ObjectFactory{
+public class CalendarTransformer extends AbstractTransformer{
 
 	public void transform(Object object) {
 		this.getContext().writeQuoted(de.unigoettingen.ct.data.io.DateUtils.calendarToString((Calendar) object));
 	}
-
-	@SuppressWarnings("rawtypes")
-	public Object instantiate(ObjectBinder context, Object value, Type targetType, Class targetClass) {
-		try{
-			return de.unigoettingen.ct.data.io.DateUtils.parseTimestamp((String) value);
-		}
-		catch(ParseException e){
-			throw new RuntimeException(e);
-		}
-	}
-	
 
 }
